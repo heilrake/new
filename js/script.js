7e7ad8f6aedddce99be8f6da86934699a4588cfa
mike <mishakvochko@ukr.net>
2021-07-09 10:25:32 +0300

    first practic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-let number = 123;
+let numberOfFilms = +prompt('сколько фильмов ви уже просмотрели?', '');
 
-console.log(number);
+const dataBase = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+
+};
+
+
+
+const a = prompt('один из последних просмотрених фильмов', ''),
+   b = prompt('на сколько оцените его', '');
+dataBase.movies[a] = b;
+console.log(dataBase)
